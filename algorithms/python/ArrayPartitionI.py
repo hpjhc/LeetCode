478566c77a2529a70786cebd104785d99420ee80
hpjhc <649140877@qq.com>
2018-10-12 13:53:50 +0800

    Create ArrayPartitionI.py

---
+++ algorithms/python/ArrayPartitionI.py
@@ -0,0 +1,14 @@
+"""
+source：https://leetcode.com/problems/arranging-coins/
+author: hpjhc
+date: 2018/10/9
+"""
+
+class Solution:
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort(reverse=True)
+        return sum(nums[1::2])
