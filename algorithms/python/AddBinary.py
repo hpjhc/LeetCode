73608a0009949c8398385249412bf26392da01db
hpjhc <649140877@qq.com>
2018-10-14 10:43:22 +0800

    Update AddBinary.py
    use builtin function int and format
    Runtime: 48 ms, faster than 77.71% of Python3 online submissions for Add Binary.

--- algorithms/python/AddBinary.py
+++ algorithms/python/AddBinary.py
@@ -5,40 +5,12 @@ date: 2018/9/22
 """
 
 class Solution:
-   def addBinary(self, a, b):
-       """
-       :type a: str
-       :type b: str
-       :rtype: str
-       """
-       res = ""
-       a = a[::-1]
-       b = b[::-1]
-       if len(a) < len(b):
-           temp = a
-           a = b
-           b = temp    
-       lens = len(a)    
-       i = 0
-       flag = 0
-       while i < lens:
-           if i < len(b):
-               num = int(a[i]) + int(b[i]) + flag    
-               if num > 1:
-                   res += str(num % 2)
-                   flag = num // 2
-               else:
-                   res += str(num)
-                   flag = 0    
-           else:
-               num = int(a[i]) + flag       
-               if num > 1:
-                   res += str(num % 2)
-                   flag = num // 2
-               else:
-                   res += str(num)
-                   flag = 0
-           i += 1
-       if flag:
-           res += '1'             
-       return res[::-1]
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        a = int(a, 2)
+        b = int(b, 2)
+        return format(a + b, 'b')
