92a228578f1033e40a0dbf0ba29e7f2d9af5051f
hpjhc <649140877@qq.com>
2018-10-15 20:36:47 +0800

    Update AddStrings.py
    much more simple solution

--- algorithms/python/AddStrings.py
+++ algorithms/python/AddStrings.py
@@ -11,25 +11,4 @@ def addStrings(self, num1, num2):
     :type num2: str
     :rtype: str
     """
-    len1 = len(num1)
-    len2 = len(num2)
-    if len1 < len2:
-        lens = len2
-        num1 = '0'*(lens-len1)+num1
-    else:
-        lens = len1   
-        num2 = '0'*(lens-len2)+num2
-    i =  lens - 1   
-    flag = 0
-    res = ""
-    while i >= 0:
-        temp = int(num1[i]) + int(num2[i]) + flag
-        res = str(temp % 10) + res
-        if temp >= 10:
-            flag = 1
-        else:
-            flag = 0 
-        i -= 1  
-    if flag:
-        res = '1' + res  
-    return res
+    return str(sum((int(num1), int(num2))))
